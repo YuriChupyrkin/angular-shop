55f7283afc5394a0b2a494f187bb6515f880ab05
Yuri Chupyrkin <yuri.chupyrkin@gmail.com>
2018-03-25 17:36:23 +0300

    Add 'ViewChildren' demo

--- src/app/product/products.component.ts
+++ src/app/product/products.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  Output,
+  EventEmitter,
+  ViewChildren,
+  ElementRef,
+} from '@angular/core';
 
 import Product from '../common/product.model';
 import { ProductsService } from './products.service';
@@ -8,14 +16,23 @@ import { ProductsService } from './products.service';
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
-export class ProductsComponent implements OnInit {
-  @Output() buyItem: EventEmitter<Product> = new EventEmitter<Product>();
+export class ProductsComponent implements OnInit, AfterViewInit {
+  @Output() buyItem: EventEmitter<Product> =
+    new EventEmitter<Product>();
+  @ViewChildren('productsItems') productsItems: Array<ElementRef> =
+    new Array<ElementRef>();
   products: Array<Product>;
 
   constructor(private productsService: ProductsService) { }
 
   ngOnInit() {
     this.products = this.productsService.loadProduct();
+  }
+
+  ngAfterViewInit() {
+    this.productsItems.forEach((item: any) => {
+      console.log(`item view inited. price: ${item.itemPrice}`);
+    });
   }
 
   onBuy(product: Product): void {
